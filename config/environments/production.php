3d8a00aeabad85ae270bbba24a57104520837a7b
Tanner Moushey <tanner@iwitnessdesign.com>
2018-08-22 23:34:02 -0700

    Update version on HC Provisional Content

--- config/environments/production.php
+++ config/environments/production.php
@@ -7,7 +7,6 @@ define('DISALLOW_FILE_MODS', true); // this disables all file modifications incl
 define('WP_REDIS_DISABLED', true);
 
 define('WP_DEBUG', true);
-define('WP_DEBUG_DISPLAY', false);
 
 ini_set('log_errors', 1);
 ini_set('error_log', getenv('WP_LOGS_DIR') . '/debug.log');
